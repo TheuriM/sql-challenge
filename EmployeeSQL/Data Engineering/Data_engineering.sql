b6809a00bd83b9b5cd56c8ed778ec64c7353560f
git config --global user.name your_username <125322343+TheuriM@users.noreply.github.com>
2024-09-14 11:58:13 -0500

    added code into data_engineering

---
+++ EmployeeSQL/Data Engineering/Data_engineering.sql
@@ -0,0 +1,61 @@
+
+-- Create titles table (employee titles)
+DROP TABLE IF EXISTS titles;
+CREATE TABLE titles (
+	title_id VARCHAR(10) PRIMARY KEY,
+	title VARCHAR(30) NOT NULL
+);
+SELECT * FROM titles;
+
+-- Create departments table
+DROP TABLE IF EXISTS departments;
+CREATE TABLE departments (
+    dept_no VARCHAR(10) PRIMARY KEY,
+    dept_name VARCHAR(30) NOT NULL
+);
+SELECT * FROM departments;
+
+-- Create employees table
+DROP TABLE IF EXISTS employees;
+CREATE TABLE employees (
+    emp_no INT PRIMARY KEY,
+	emp_title_id VARCHAR(5),
+    birth_date DATE NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    sex VARCHAR(5) NOT NULL,
+    hire_date DATE NOT NULL,
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+SELECT * FROM employees;
+
+-- Create salaries table
+DROP TABLE IF EXISTS salaries;
+CREATE TABLE salaries (
+    emp_no INT NOT NULL,
+    salary INT NOT NULL,
+    CONSTRAINT pk_salaries PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+SELECT * FROM salaries;
+
+-- Create dept_emp table (relationship between employees and departments)
+DROP TABLE IF EXISTS dept_emp;
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(10) NOT NULL,
+	PRIMARY KEY (emp_no, dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+SELECT * FROM dept_emp;
+
+-- Create dept_manager table (managers of departments)
+DROP TABLE IF EXISTS dept_manager;
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(10) NOT NULL,
+	emp_no INT PRIMARY KEY,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+SELECT * FROM dept_manager;
